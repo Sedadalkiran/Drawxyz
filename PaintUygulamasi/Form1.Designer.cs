e25243f8f511751916dfea0df1eff9e1b7f6c4c6
Sedadalkiran <46977542+Sedadalkiran@users.noreply.github.com>
2019-01-29 00:51:20 +0300

    Draw
    Çizim Programı

---
+++ PaintUygulamasi/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace PaintUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(39, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Renk";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "20"});
+            this.comboBox1.Location = new System.Drawing.Point(41, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(50, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.MouseEnter += new System.EventHandler(this.comboBox1_MouseEnter);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(18, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 20);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(44, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 20);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(70, 30);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(20, 20);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(11, 29);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(20, 20);
+            this.button5.TabIndex = 2;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            this.button5.MouseEnter += new System.EventHandler(this.button5_MouseEnter);
+            // 
+            // button7
+            // 
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(13, 30);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(20, 20);
+            this.button7.TabIndex = 2;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button4_Click);
+            this.button7.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "3",
+            "5",
+            "6",
+            "8",
+            "10",
+            "20",
+            "35"});
+            this.comboBox2.Location = new System.Drawing.Point(37, 29);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(42, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.MouseEnter += new System.EventHandler(this.comboBox2_MouseEnter);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(15, 29);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(20, 20);
+            this.button8.TabIndex = 2;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button4_Click);
+            this.button8.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Location = new System.Drawing.Point(2, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(122, 65);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fırça Kalınlığı";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Location = new System.Drawing.Point(130, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(125, 65);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Renk Seç";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Controls.Add(this.button2);
+            this.groupBox4.Location = new System.Drawing.Point(261, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(99, 65);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Şekil Aracı";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox5.Controls.Add(this.comboBox2);
+            this.groupBox5.Controls.Add(this.button5);
+            this.groupBox5.Location = new System.Drawing.Point(366, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(90, 65);
+            this.groupBox5.TabIndex = 8;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Silgi Aracı";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Location = new System.Drawing.Point(462, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(74, 65);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kaydet";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(7, 29);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(62, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Kaydet";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox6.Controls.Add(this.button9);
+            this.groupBox6.Location = new System.Drawing.Point(542, 12);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(74, 65);
+            this.groupBox6.TabIndex = 8;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Yeni";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(7, 29);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(62, 23);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "Yeni Oluştur";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(622, 518);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Draw";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ PaintUygulamasi/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string yap;
+       public  enum cizilen
+        {
+            kare,
+            elips,
+            silgi,
+            cizgi
+
+
+        }
+        int silgikalinlik = 3;
+
+
+
+        bool draw;
+        int startX, startY;
+        int thickness = 3;
+        ColorDialog choosecolor = new ColorDialog();
+
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            draw = true;
+            startX = e.X;
+            startY = e.Y;
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Graphics g = this.CreateGraphics();
+            Pen p = new Pen(choosecolor.Color, thickness);
+            Point point1 = new Point(startX,startY);
+            Point point2 = new Point(e.X,e.Y);
+            
+            Rectangle kare = new Rectangle(startX,startY,200,200);
+            Rectangle elipss = new Rectangle(startX, startY, 200,200);
+            
+            
+            if (draw == true)
+            {
+                if (yap == cizilen.cizgi.ToString())
+                {
+                g.DrawLine(p, point1, point2);
+                startX = e.X;
+                startY = e.Y;
+                }
+                if(yap==cizilen.kare.ToString())
+                {
+                    
+                    g.DrawRectangle(p, kare);
+                   
+                    
+                    startX = e.X;
+                    startY = e.Y;
+                    draw = false;
+                    
+                }
+                if (yap == cizilen.elips.ToString())
+                {
+                    g.DrawEllipse(p, elipss);
+                    startX = e.X;
+                    startY = e.Y;
+                    draw = false;
+
+
+                }
+                if(yap==cizilen.silgi.ToString())
+                {
+                    Pen yeni = new Pen(Color.White, silgikalinlik);
+                    g.DrawLine(yeni, point1, point2);
+                    startX = e.X;
+                    startY = e.Y;
+                }
+
+                else
+                {
+                    g.DrawLine(p, point1, point2);
+                    startX = e.X;
+                    startY = e.Y;
+
+                }
+                
+
+            }
+
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            choosecolor.ShowDialog();
+
+           
+                button7.BackColor = choosecolor.Color;
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            thickness = int.Parse(comboBox1.SelectedItem.ToString());
+        }
+
+        //private void comboBox1_MouseHover(object sender, EventArgs e)
+        //{
+        //    ToolTip message = new ToolTip();
+        //    message.SetToolTip(comboBox1, "Fırça kalınlığını seçiniz.");
+          
+        //    message.ToolTipIcon = ToolTipIcon.Info;
+        //}
+
+        private void comboBox1_MouseEnter(object sender, EventArgs e)
+        {
+
+            ToolTip message = new ToolTip();
+            message.SetToolTip(comboBox1, "Fırça kalınlığını seçiniz.");
+
+            message.ToolTipIcon = ToolTipIcon.Info;
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+
+            ToolTip message = new ToolTip();
+            message.SetToolTip(button2, "Çizgi");
+
+            message.ToolTipIcon = ToolTipIcon.Info;
+
+        }
+
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            ToolTip mesaj = new ToolTip();
+            mesaj.SetToolTip(button3, "Elips");
+            mesaj.ToolTipIcon = ToolTipIcon.Info;
+
+
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            ToolTip mesaj = new ToolTip();
+            mesaj.SetToolTip(button4, "Kare");
+            mesaj.ToolTipIcon = ToolTipIcon.Info;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          yap=Convert.ToString(cizilen.cizgi);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            yap = Convert.ToString(cizilen.elips);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            yap = Convert.ToString(cizilen.kare);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            yap = Convert.ToString(cizilen.silgi);
+        }
+
+        private void button5_MouseEnter(object sender, EventArgs e)
+        {
+            ToolTip mesaj = new ToolTip();
+            mesaj.SetToolTip(button3, "Silgi");
+            mesaj.ToolTipIcon = ToolTipIcon.Info;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            silgikalinlik = int.Parse(comboBox2.SelectedItem.ToString());
+        }
+
+        private void comboBox2_MouseEnter(object sender, EventArgs e)
+        {
+            ToolTip mesaj = new ToolTip();
+            mesaj.SetToolTip(comboBox2, "Silgi boyutu");
+            mesaj.ToolTipIcon = ToolTipIcon.Info;
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           
+            Bitmap screenshot = new Bitmap(this.Width-20, this.Height-120);
+            Graphics grafik = Graphics.FromImage((Image)screenshot);
+            grafik.CopyFromScreen(this.Location.X+10, this.Location.Y+110, 0, 0, this.Size);
+           string isim = Path.GetRandomFileName();
+            screenshot.Save("d://"+isim+".png", ImageFormat.Png);
+            MessageBox.Show("Dosya " + isim + "ismiyle kaydedildi.");
+            
+
+        }
+
+      
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            yap = Convert.ToString(cizilen.silgi);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Graphics a = this.CreateGraphics();
+            a.Clear(Form.DefaultBackColor);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button7.BackColor = Color.Black;
+        }
+    }
+}
